6ab58929589954549c44a2f0140558f867936898
Tomohide Sawada <ma1n.tmrev01ut10n@gmail.com>
2024-11-27 12:42:27 +0900

    Update: 環境変数取得処理のリファクタリング

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -13,33 +13,33 @@ class HomeController extends Controller
         if (is_null($city)) {
             $this->render('index');
         } else {
-            if (isset(getenv()['DATABASE_HOST'])) {
-                $host = getenv()['DATABASE_HOST'];
+            if (!empty(getenv('DATABASE_HOST'))) {
+                $host = getenv('DATABASE_HOST');
             } else {
                 $host = "localhost";
             }
-            if (isset(getenv()['DATABASE_USER'])) {
-                $user = getenv()['DATABASE_USER'];
+            if (!empty(getenv('DATABASE_USER'))) {
+                $user = getenv('DATABASE_USER');
             } else {
                 $user = "mongo";
             }
-            if (isset(getenv()['DATABASE_PASSWORD'])) {
-                $password = getenv()['DATABASE_PASSWORD'];
+            if (!empty(getenv('DATABASE_PASSWORD'))) {
+                $password = getenv('DATABASE_PASSWORD');
             } else {
                 $password = "mongo";
             }
-            if (isset(getenv()['DATABASE_PORT'])) {
-                $port = getenv()['DATABASE_PORT'];
+            if (!empty(getenv('DATABASE_PORT'))) {
+                $port = getenv('DATABASE_PORT');
             } else {
                 $port = "27017";
             }
-            if (isset(getenv()['DATABASE_NAME'])) {
-                $dbname = getenv()['DATABASE_NAME'];
+            if (!empty(getenv('DATABASE_NAME'))) {
+                $dbname = getenv('DATABASE_NAME');
             } else {
                 $dbname = "zipsdb";
             }
-            if (isset(getenv()['DATABASE_COLLECTION'])) {
-                $collection = getenv()['DATABASE_COLLECTION'];
+            if (!empty(getenv('DATABASE_COLLECTION'))) {
+                $collection = getenv('DATABASE_COLLECTION');
             } else {
                 $collection = "zips";
             }
