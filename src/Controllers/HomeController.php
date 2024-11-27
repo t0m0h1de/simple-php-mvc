e3ee9877d76e3caa043933e3518d91393c97aee2
Tomohide Sawada <ma1n.tmrev01ut10n@gmail.com>
2024-11-27 11:11:38 +0900

    Update: 環境変数から接続情報を取るように変更

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -13,7 +13,37 @@ class HomeController extends Controller
         if (is_null($city)) {
             $this->render('index');
         } else {
-            $repo = new ZipsMongo("mongodb://mongo:mongo@localhost:27017", "zipsdb", "zips");
+            if (isset(getenv()['DATABASE_HOST'])) {
+                $host = getenv()['DATABASE_HOST'];
+            } else {
+                $host = "localhost";
+            }
+            if (isset(getenv()['DATABASE_USER'])) {
+                $user = getenv()['DATABASE_USER'];
+            } else {
+                $user = "mongo";
+            }
+            if (isset(getenv()['DATABASE_PASSWORD'])) {
+                $password = getenv()['DATABASE_PASSWORD'];
+            } else {
+                $password = "mongo";
+            }
+            if (isset(getenv()['DATABASE_PORT'])) {
+                $port = getenv()['DATABASE_PORT'];
+            } else {
+                $port = "27017";
+            }
+            if (isset(getenv()['DATABASE_NAME'])) {
+                $dbname = getenv()['DATABASE_NAME'];
+            } else {
+                $dbname = "zipsdb";
+            }
+            if (isset(getenv()['DATABASE_COLLECTION'])) {
+                $collection = getenv()['DATABASE_COLLECTION'];
+            } else {
+                $collection = "zips";
+            }
+            $repo = new ZipsMongo("mongodb://$user:$password@$host:$port", $dbname, $collection);
             $zip = $repo->findByName(strtoupper($city));
             $this->render('index', ['zip' => $zip]);
         }
